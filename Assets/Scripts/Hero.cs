8bcb21887b05c88338ee4a870814819503ab2616
yarik <yarik.resanov@gmail.com>
2022-10-21 17:19:16 +0300

    added attacks,enemies and dialogues
    1.added attacks with animations
    2.added entities and enemies
    3.added a system of dialogues with animations
    4.some fixes

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hero : MonoBehaviour
+public class Hero : Entity
 {
    [SerializeField] private float speed = 3f;
-   [SerializeField] private int lives = 5;
-   [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float jumpForce = 5f;
    private bool isGrounded = false;
-   
+
+   public bool isAttacking = false;
+   public bool isRecharged = true;
+
+   public Transform attackPos;
+   public float attackRange;
+   public LayerMask enemy;
+
    private Rigidbody2D rb;
    private Animator anim;
    private SpriteRenderer sprite;
+   
+   public static Hero Instance {get;set;}
 
    private States State
    {
@@ -24,7 +32,10 @@ public class Hero : MonoBehaviour
     rb = GetComponent<Rigidbody2D>();
     anim = GetComponent<Animator>();
     sprite = GetComponentInChildren<SpriteRenderer>();    
-   }
+    Instance = this;
+    isRecharged = true;
+    lives = 5;
+    }
    private void FixedUpdate()
    {
       CheckGround();
@@ -32,12 +43,14 @@ public class Hero : MonoBehaviour
 
    private void Update()
    {
-      if (isGrounded) State = States.idle;
-    if (Input.GetButton("Horizontal"))
+      if (isGrounded && !isAttacking) State = States.idle;
+    if (!isAttacking && Input.GetButton("Horizontal"))
         Run();
 
-    if (isGrounded && Input.GetButtonDown("Jump"))
+    if (!isAttacking && isGrounded && Input.GetButtonDown("Jump"))
       Jump();
+        if (Input.GetButtonDown("Fire1"))
+            Attack();
    }
 
    private void Run()
@@ -54,18 +67,60 @@ public class Hero : MonoBehaviour
       rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
    }
 
-   private void CheckGround()
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(attackPos.position, attackRange);
+    }
+    private void CheckGround()
    {
       Collider2D[] collider =Physics2D.OverlapCircleAll(transform.position, 0.3f);
       isGrounded = collider.Length >1;
       
       if (!isGrounded) State = States.jump;
    }
+  
+    private void Attack()
+    { if (isGrounded && isRecharged)
+        {
+            State = States.attack;
+            isAttacking = true;
+            isRecharged = false;
+
+            StartCoroutine(AttackAnimation());
+            StartCoroutine(AttackCoolDown());
+        }
+    
+                }
+    private void OnAttack()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemy);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            colliders[i].GetComponent<Entity>().GetDamage();
+        }
+    }
+    
+    private IEnumerator AttackAnimation()
+    {
+        yield return new WaitForSeconds(0.9f);
+        isAttacking = false;
+    }    
+    private IEnumerator AttackCoolDown()
+    {
+        yield return new WaitForSeconds(0.9f);
+        isRecharged = true;
+    }
 }
+
+  
+
+   
 
 public enum States
 {
    idle,
    run,
-   jump
+   jump,
+   attack
 }
