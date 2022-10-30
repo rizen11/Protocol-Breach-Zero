2b6211927b579e6bf2aa04a165404b08f21e5c25
z0tedd <z0tedd@gmail.com>
2022-10-30 21:19:41 +0300

    was fixed Main menu, added propety for switching levels in main menu.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+
+	int m_LevelNumber = 2; //Переменна, отвечающая за то, какой уровень загружать.
 	public void PlayGame()
 	{
-	 	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	 	SceneManager.LoadScene(LevelNumber); //Загружает сцену один(см. BuildSettings) Решение для загрузики последующих уровней:(SceneManager.GetActiveScene().buildIndex + 1);
 	}
 	public void ExitGame()
 	{
 		Debug.Log("Пацаны зацените че могу");
 		Application.Quit();
 	}
-
+	
+	//Св-во для изменения переменной, отвечающей за прогрузку уровней
+	public int LevelNumber
+	{
+		get {return m_LevelNumber;}
+		set {m_LevelNumber = value;}
+	
+	}
 
 }
